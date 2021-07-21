f74677e90d1ac2800bc62eccc0a446a764dede55
jgmdev <jgmdev@gmail.com>
2021-07-21 14:56:19 -0400

    Added appimage build script.

---
+++ scripts/build-appimage.sh
@@ -0,0 +1,97 @@
+#!/bin/env bash
+
+if [ ! -e "src/api/api.h" ]; then
+  echo "Please run this script from the root directory of Lite XL."
+  exit 1
+fi
+
+ARCH="$(uname -m)"
+APPIMAGETOOL=appimagetool
+
+setup_appimagetool(){
+  if which appimagetool > /dev/null ; then
+    if [ ! -e appimagetool ]; then
+      if ! wget -O appimagetool "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-${ARCH}.AppImage" ; then
+        echo "Could not download the appimagetool for the arch '${ARCH}'."
+        exit 1
+      else
+        chmod 0755 appimagetool
+        APPIMAGETOOL="./appimagetool"
+      fi
+    fi
+  fi
+}
+
+download_appimage_apprun(){
+  if [ ! -e AppRun ]; then
+    if ! wget -O AppRun "https://github.com/AppImage/AppImageKit/releases/download/continuous/AppRun-${ARCH}" ; then
+      echo "Could not download AppRun for the arch '${ARCH}'."
+      exit 1
+    else
+      chmod 0755 AppRun
+    fi
+  fi
+}
+
+build_litexl(){
+  if [ -e build ]; then
+    rm -rf build
+  fi
+  
+  echo "Build lite-xl..."
+  sleep 1
+  meson setup --prefix=/usr build
+  meson compile -C build
+}
+
+generate_appimage(){
+  if [ -e LiteXL.AppDir ]; then
+    rm -rf LiteXL.AppDir
+  fi
+  
+  echo "Creating LiteXL.AppDir..."
+  
+  mkdir LiteXL.AppDir
+  DESTDIR="$(realpath LiteXL.AppDir)" meson install --skip-subprojects -C build
+  mkdir -p LiteXL.AppDir/usr/lib/
+  mkdir -p LiteXL.AppDir/usr/share/icons/hicolor/scalable/apps
+  mkdir -p LiteXL.AppDir/usr/share/applications
+  cp AppRun LiteXL.AppDir/
+  cp resources/icons/lite-xl.svg LiteXL.AppDir/
+  cp resources/icons/lite-xl.svg LiteXL.AppDir/usr/share/icons/hicolor/scalable/apps/
+  cp resources/linux/lite-xl.desktop LiteXL.AppDir/
+  cp resources/linux/lite-xl.desktop LiteXL.AppDir/usr/share/applications/
+  
+  echo "Copying libraries..."
+  
+  local ignore_libs=(
+    X11
+    linux-vdso
+    libstdc++.so
+    libm.so
+    libgcc_s.so
+    libpthread.so
+    libc.so
+    linux-
+    libdl.so
+    libglib
+  )
+
+  while read line; do
+    for lib in "${ignore_libs[@]}" ; do
+      if echo "$line" | grep "$lib" > /dev/null ; then
+        echo "  Ignoring: $line"
+        continue 2
+      fi
+    done
+    cp "/usr/lib/$line" LiteXL.AppDir/usr/lib/
+  done < <(ldd build/src/lite-xl | awk "{print \$1}")
+  
+  echo "Generating AppImage..."
+  $APPIMAGETOOL LiteXL.AppDir LiteXL-${ARCH}.AppImage
+}
+
+setup_appimagetool
+download_appimage_apprun
+build_litexl
+generate_appimage
