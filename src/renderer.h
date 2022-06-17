b314a010a1839031a32028730474e927c14764d1
jgmdev <jgmdev@gmail.com>
2022-06-17 11:24:52 -0400

    support for fractional scaling

--- src/renderer.h
+++ src/renderer.h
@@ -11,13 +11,19 @@
 #define UNUSED
 #endif
 
+#ifdef LITE_USE_SDL_RENDERER
+#define RECT_TYPE float
+#else
+#define RECT_TYPE int
+#endif
+
 #define FONT_FALLBACK_MAX 10
 typedef struct RenFont RenFont;
 typedef enum { FONT_HINTING_NONE, FONT_HINTING_SLIGHT, FONT_HINTING_FULL } ERenFontHinting;
 typedef enum { FONT_ANTIALIASING_NONE, FONT_ANTIALIASING_GRAYSCALE, FONT_ANTIALIASING_SUBPIXEL } ERenFontAntialiasing;
 typedef enum { FONT_STYLE_BOLD = 1, FONT_STYLE_ITALIC = 2, FONT_STYLE_UNDERLINE = 4, FONT_STYLE_SMOOTH = 8, FONT_STYLE_STRIKETHROUGH = 16 } ERenFontStyle;
 typedef struct { uint8_t b, g, r, a; } RenColor;
-typedef struct { int x, y, width, height; } RenRect;
+typedef struct { RECT_TYPE x, y, width, height; } RenRect;
 
 RenFont* ren_font_load(const char *filename, float size, ERenFontAntialiasing antialiasing, ERenFontHinting hinting, unsigned char style);
 RenFont* ren_font_copy(RenFont* font, float size, ERenFontAntialiasing antialiasing, ERenFontHinting hinting, int style);
@@ -29,7 +35,7 @@ float ren_font_group_get_size(RenFont **font);
 void ren_font_group_set_size(RenFont **font, float size);
 void ren_font_group_set_tab_size(RenFont **font, int n);
 float ren_font_group_get_width(RenFont **font, const char *text, size_t len);
-float ren_draw_text(RenFont **font, const char *text, size_t len, float x, int y, RenColor color);
+float ren_draw_text(RenFont **font, const char *text, size_t len, float x, float y, RenColor color);
 
 void ren_draw_rect(RenRect rect, RenColor color);
 
@@ -39,6 +45,7 @@ void ren_update_rects(RenRect *rects, int count);
 void ren_set_clip_rect(RenRect rect);
 void ren_get_size(int *x, int *y); /* Reports the size in points. */
 void ren_free_window_resources();
+float ren_get_scale_factor(SDL_Window *win);
 
 
 #endif
