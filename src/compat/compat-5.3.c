3683f983fc977cf0a6af546ca5f0dbf2cf55ff1d
Sergii Fesenko <sergii_f@outlook.com>
2022-10-20 12:11:25 +0300

    add `luaL_typeerror` to fix compilation

--- src/compat/compat-5.3.c
+++ src/compat/compat-5.3.c
@@ -922,6 +922,18 @@ COMPAT53_API void luaL_requiref (lua_State *L, const char *modname,
   lua_replace(L, -2);
 }
 
+COMPAT53_API int luaL_typeerror (lua_State *L, int arg, const char *tname) {
+  const char *msg;
+  const char *typearg;  /* name for the type of the actual argument */
+  if (luaL_getmetafield(L, arg, "__name") == LUA_TSTRING)
+    typearg = lua_tostring(L, -1);  /* use the given type name */
+  else if (lua_type(L, arg) == LUA_TLIGHTUSERDATA)
+    typearg = "light userdata";  /* special name for messages */
+  else
+    typearg = luaL_typename(L, arg);  /* standard name */
+  msg = lua_pushfstring(L, "%s expected, got %s", tname, typearg);
+  return luaL_argerror(L, arg, msg);
+}
 
 #endif /* Lua 5.1 and 5.2 */
 
