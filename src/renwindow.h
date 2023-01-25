ea63368b362088d1c33def64d9567b9180a83ae0
jgmdev <jgmdev@gmail.com>
2023-01-25 17:03:53 -0400

    merged improved-scale branch

--- src/renwindow.h
+++ src/renwindow.h
@@ -4,17 +4,16 @@
 struct RenWindow {
   SDL_Window *window;
   RenRect clip; /* Clipping rect in pixel coordinates. */
-#ifdef LITE_USE_SDL_RENDERER
   SDL_Renderer *renderer;
   SDL_Texture *texture;
   SDL_Surface *surface;
-  int surface_scale;
-#endif
+  float surface_scale_x;
+  float surface_scale_y;
 };
 typedef struct RenWindow RenWindow;
 
 void renwin_init_surface(RenWindow *ren);
-int  renwin_surface_scale(RenWindow *ren);
+void renwin_surface_scale(RenWindow *ren, float *scale_x, float *scale_y);
 void renwin_clip_to_surface(RenWindow *ren);
 void renwin_set_clip_rect(RenWindow *ren, RenRect rect);
 void renwin_resize_surface(RenWindow *ren);
@@ -22,4 +21,3 @@ void renwin_show_window(RenWindow *ren);
 void renwin_update_rects(RenWindow *ren, RenRect *rects, int count);
 void renwin_free(RenWindow *ren);
 SDL_Surface *renwin_get_surface(RenWindow *ren);
-
